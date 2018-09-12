41c23c67bdfe9ba9525a15a184b065a791d375a3
Jantastication O <georginaoregx@gmail.com>
2018-09-12 21:57:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,9 @@
 def display_board
-   puts "Welcome to Tic Tac Toe"
    puts "   |   |   "
    puts "-----------"
    puts "   |   |   "
    puts "-----------"
    puts "   |   |   "
-   puts "prints a 3x3 tic tac toe board"
 end
-
 display_board
+
