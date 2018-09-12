1f09c07ca4eb5ea0bf313788ed991296fd4f4b1e
Jantastication O <georginaoregx@gmail.com>
2018-09-12 20:32:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 def display_board
-   puts "A Tic Tac Toe Board"
+   puts "represents a cell as a string with 3 spaces"
+   puts "separates cells with a | character"
+   puts "prints a 3 cell row"
+   puts "seperates rows with a line of 11 -"
+   puts "prints a 3x3 tic tac toe board"
+end
+
+display_board
